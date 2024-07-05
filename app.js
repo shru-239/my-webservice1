37423023c820adb15a080f145341b5461ea551da
shru-239 <135866114+shru-239@users.noreply.github.com>
2024-07-05 19:31:46 +0530

    done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'view'))); // To serve static files
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/webserviceDb', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/webserviceDb', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Routes
 app.use('/users', usersRouter);
